388494ba8716c7dd62fd9457220c4d1ae325e397
Kurt Lekanger <kurt@lekanger.no>
2020-01-01 12:38:53 +0100

    Added contact info at the bottom of all pages.

---
+++ web/src/components/contact-info.js
@@ -0,0 +1,26 @@
+// Contact info at the bottom of all pages
+
+import React from 'react'
+import styles from './contact-info.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+const ContactInfo = () => {
+  const twitter = <a href='https://twitter.com/lekanger'>klekanger</a>
+  const github = <a href='https://github.com/klekanger'> Github</a>
+  const linkedin = <a href='https://www.linkedin.com/in/lekanger'> LinkedIn</a>
+
+  return (
+    <div className={styles.infotxt}>
+      <p><FontAwesomeIcon className={styles.icn} icon={faPhone} /> <a href='tel:+4740210140'>+47 - 40 21 01 40</a> </p>
+      <p><FontAwesomeIcon className={styles.icn} icon={faEnvelope} /><a href='mailto:kurt@lekanger.no'> kurt@lekanger.no</a></p>
+      <p><FontAwesomeIcon className={styles.icn} icon={faTwitter} /> {twitter}</p>
+      <p><FontAwesomeIcon className={styles.icn} icon={faGithub} />{github}</p>
+      <p><FontAwesomeIcon className={styles.icn} icon={faLinkedin} />{linkedin}</p>
+    </div>
+
+  )
+}
+
+export default ContactInfo
--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Header from './header'
+import ContactInfo from './contact-info'
 
 import '../styles/layout.css'
 import styles from './layout.module.css'
 
-const Layout = ({children, onHideNav, onShowNav, showNav, siteTitle}) => (
+const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => (
   <>
     <Header siteTitle={siteTitle} onHideNav={onHideNav} onShowNav={onShowNav} showNav={showNav} />
     <div className={styles.content}>{children}</div>
     <footer className={styles.footer}>
       <div className={styles.footerWrapper}>
+        <ContactInfo />
         <div className={styles.siteInfo}>
           © Kurt Lekanger {new Date().getFullYear()}, Laget med <a href='https://www.sanity.io'>Sanity</a> &amp;
           {` `}
