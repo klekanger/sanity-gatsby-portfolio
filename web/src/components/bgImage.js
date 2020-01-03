2d42ec63b51b40e09a977fd6e22c29dfd4d85e7f
Kurt Lekanger <kurt@lekanger.no>
2020-01-03 17:51:36 +0100

    Converted background to webp for quicker page load

--- web/src/components/bgImage.js
+++ web/src/components/bgImage.js
@@ -6,7 +6,7 @@ import styles from './bgImage.module.css'
 const BgImage = ({ children }) => {
   const data = useStaticQuery(graphql`
     {
-      file(relativePath: {eq: "background3.jpg"}) {
+      file(relativePath: {eq: "background3.webp"}) {
         childImageSharp {
           fluid(maxWidth:2000, quality: 100) {
             ...GatsbyImageSharpFluid
