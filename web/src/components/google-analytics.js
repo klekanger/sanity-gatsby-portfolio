68f59d515e7362c0513645ad7712c4bfeec2da82
Kurt Lekanger <kurt@lekanger.no>
2020-01-05 14:33:43 +0100

    Google Analytics

--- web/src/components/google-analytics.js
+++ web/src/components/google-analytics.js
@@ -6,13 +6,13 @@ const GoogleAnalytics = () => {
       dangerouslySetInnerHTML={{
         __html: `
         <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-6852843-1"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-6852843-2"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
         
-          gtag('config', 'UA-6852843-1');
+          gtag('config', 'UA-6852843-2');
         </script>
         `
       }}
