9ecec18c353fd9058b222271de963d50d624afe5
Kurt Lekanger <kurt@lekanger.no>
2020-01-11 11:48:27 +0100

    Replaced deprecated anim lib with Framer Motion

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -2,31 +2,28 @@ import { Link } from 'gatsby'
 import React from 'react'
 import Icon from './icon'
 import { cn } from '../lib/helpers'
+import { motion } from "framer-motion"
 
 import styles from './header.module.css'
+
 
 const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => (
   <div className={styles.root}>
     <div className={styles.wrapper}>
       <div className={styles.branding}>
-        <Link to='/'><span className={styles.logoColor1}>{'{'} kurt_</span><span className={styles.logoColor2}>lekanger {'}'}</span></Link>
+        <motion.div
+          initial={{ y: -150, opacity: 0.5 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 250,
+            damping: 20
+          }}
+        >
+          <Link to='/'><span className={styles.logoColor1}>{'{'} kurt_</span><span className={styles.logoColor2}>lekanger {'}'}</span></Link>
+        </motion.div>
         <div className={styles.navMenu}><a href='https://linkedin.com/in/lekanger'>LinkedIn</a> –<a href='https://github.com/klekanger'>Github</a></div>
       </div>
-
-      {/*  --- commented out the nav/hamburger menu
-      <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
-        <Icon symbol='hamburger' />
-      </button>
-
-      <nav className={cn(styles.nav, showNav && styles.showNav)}>
-        <ul>
-          <li>
-            <Link to='/archive/'>Arkiv</Link>
-          </li>
-        </ul>
-      </nav>
-          */}
-
     </div>
   </div >
 )
--- web/src/components/project-preview.js
+++ web/src/components/project-preview.js
@@ -5,7 +5,7 @@ import { imageUrlFor } from '../lib/image-url'
 import BlockText from './block-text'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
-import posed from 'react-pose';
+import { motion } from "framer-motion"
 
 import styles from './project-preview.module.css'
 import { responsiveTitle3 } from './typography.module.css'
@@ -18,24 +18,17 @@ function ProjectPreview(props) {
   )
     : null
 
-  const ImgBox = posed.div({
-    hoverable: true,
-    pressable: false,
-    init: {
-      scale: 1,
-      boxShadow: '0px 0px 0px rgba(0,0,0,0)',
-      transition: { duration: 100 }
-    },
-    hover: {
-      scale: 1.05,
-      boxShadow: '0px 5px 10px rgba(0,0,0,0.2)',
-      transition: { duration: 100 }
-    }
-  });
+
 
   return (
     <Link className={styles.root} to={`/project/${props.slug.current}`}>
-      <ImgBox className={styles.leadMediaThumb}>
+      <motion.div
+        whileHover={{
+          scale: 1.07,
+          transition: { duration: 0.2 },
+        }}
+        whileTap={{ scale: 1.0 }}
+        className={styles.leadMediaThumb}>
         {props.mainImage && props.mainImage.asset && (
           <img className={styles.previewImg}
             src={imageUrlFor(buildImageObj(props.mainImage))
@@ -45,7 +38,7 @@ function ProjectPreview(props) {
             alt={props.mainImage.alt}
           />
         )}
-      </ImgBox>
+      </motion.div>
       <h3 className={cn(responsiveTitle3, styles.title)}>
         {showPinIcon}
         {props.title}
