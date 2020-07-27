760852d6ab59a54082fc2569b8ce851353315c3c
Kurt Lekanger <kurt@lekanger.no>
2020-07-27 19:20:45 +0200

    Added syntax highlighting for code blocks

--- web/src/components/code-block.js
+++ web/src/components/code-block.js
@@ -5,20 +5,10 @@ import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-const CodeBlock = (props) => {
-  return (
-    <div
-      style={{
-        backgroundColor: '#333',
-        padding: '10px',
-        overflowX: 'scroll',
-      }}
-    >
-      <SyntaxHighlighter language={props.node.language} style={atomOneDark}>
-        {props.node.code}
-      </SyntaxHighlighter>
-    </div>
-  );
-};
+const CodeBlock = (props) => (
+  <SyntaxHighlighter language={props.node.language} style={atomOneDark}>
+    {props.node.code}
+  </SyntaxHighlighter>
+);
 
 export default CodeBlock;
