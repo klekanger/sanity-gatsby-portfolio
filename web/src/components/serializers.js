ed131e5459c7fa274552f887654cb7b706280268
Kurt Lekanger <kurt@lekanger.no>
2020-07-27 09:31:43 +0200

    Added serializer for code blocks

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -1,9 +1,15 @@
-import Figure from './figure'
+import React from "react";
+import Figure from "./figure";
 
 const serializers = {
   types: {
-    figure: Figure
-  }
-}
+    figure: Figure,
+    code: (props) => (
+      <pre data-language={props.node.language} style={{ backgroundColor: "#333", padding: "10px" }}>
+        <code>{props.node.code}</code>
+      </pre>
+    ),
+  },
+};
 
-export default serializers
+export default serializers;
